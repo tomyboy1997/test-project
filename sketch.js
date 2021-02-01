db2df6cd380f139a5b550ff01c766bcc7b2e6315
Tom Chalancon <tomchalancon@MBP-de-Tom.fritz.box>
2021-02-01 15:48:21 +0100

    on part en objet

--- sketch.js
+++ sketch.js
@@ -37,26 +37,6 @@ function scaling(x, y){
   circle(x, y, bite)
 }
 
-//well, creates a rectangle
-const createRectangle = (r, g, b, x, y, w, rectMove) => {
-  noStroke();
-  fill(r, g, b);
-  rect(width / x, height / y, w);
-  if (rectMove) {
-    rectMove()
-  }
-}
-
-// dynamic rectangle following the mouse
-const rectMove = () => {
-    let a = map(mouseX, 0, width, 0, 350);
-    fill(140, 20, 110);
-    quad(a, 120, 200, 200, 350, 200, a+150, 120);
-      //left
-    fill(30, 30, 210);
-    quad(a, 120, a, 250, 200, 350, 200, 200);
-}
-
 //background
 function back() {
   noStroke();
@@ -67,12 +47,88 @@ function back() {
   rect(width / 2, 0, 350, 700);
 }
 
+//well, creates a rectangle
+const createRectangle = (colors, coordinates, rectMove) => {
+  const {r, g, b} = colors;
+  const {x, y, w} = coordinates;
+
+  noStroke();
+  fill(r, g, b);
+  rect(width / x, height / y, w);
+  if (rectMove) {
+    rectMove()
+  }
+}
+
 function draw() {
   back();
-  const topLeft = createRectangle(30, 190, 40, 3.5, 3.5, 150, rectMove);
-  const topRight = createRectangle(30, 30, 210, 2, 3.5, 150);
-  const bottomLeft = createRectangle(210, 30, 10, 3.5, 2, 150);
-  const bottomRight = createRectangle(60, 255, 160, 2, 2, 150);
+  const rectangles = [
+  //topLeft
+    {
+      colors: {
+        r: 30,
+        g: 190,
+        b: 40
+      },
+      coordinates: {
+        x: 3.5,
+        y: 3.5,
+        w: 150
+      }
+    },
+  //topRight
+    {
+      colors: {
+        r: 30,
+        g: 30,
+        b: 210
+      },
+      coordinates: {
+        x: 2,
+        y: 3.5,
+        w: 150
+      },
+// dynamic rectangle following the mouse
+      rectMove: () => {
+        let a = map(mouseX, 0, width, 0, 350);
+        fill(140, 20, 110);
+        quad(a, 120, 200, 200, 350, 200, a+150, 120);
+          //left
+        fill(30, 30, 210);
+        quad(a, 120, a, 250, 200, 350, 200, 200);
+      }
+    },
+  //bottomLeft
+    {
+      colors: {
+        r: 210,
+        g: 30,
+        b: 10
+      },
+      coordinates: {
+        x: 3.5,
+        y: 2,
+        w: 150
+      }
+    },
+  //bottomRight
+  {
+    colors: {
+      r: 60,
+      g: 255,
+      b: 160
+    },
+    coordinates: {
+      x: 2,
+      y: 2,
+      w: 150
+    }
+  },
+]
+
+  for (const rect of rectangles) {
+    createRectangle(rect.colors, rect.coordinates, rect.rectMove)
+}
   scaling(275, 275);
   spinning(425, 425);
 }
